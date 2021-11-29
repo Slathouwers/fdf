59833e91995223196241abdce329ed0b0bab81df
Slathouwers <sander.lathouwers@live.be>
2021-11-29 08:45:36 +0000

    ADD: window dimension macros

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 11:42:04 by slathouw          #+#    #+#             */
-/*   Updated: 2021/11/29 08:25:41 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/11/29 08:45:07 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,9 @@
 # include <errno.h>
 # include <fcntl.h>
 # include <stdlib.h>
+
+# define HEIGHT			1080
+# define WIDTH			1920
+# define MENU_WIDTH		250
 
 #endif
