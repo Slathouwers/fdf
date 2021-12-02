ae39da168ff93e39e60c5b4b43aaa97cd27a6387
Slathouwers <sander.lathouwers@live.be>
2021-12-02 14:12:08 +0000

    FIX: refactored color functions into color.c

---
+++ srcs/color.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 09:21:50 by slathouw          #+#    #+#             */
+/*   Updated: 2021/12/02 09:21:58 by slathouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+/*COLOR*/
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	get_t(int trgb)
+{
+	return (trgb & (0xFF << 24));
+}
+
+int	get_r(int trgb)
+{
+	return (trgb & (0xFF << 16));
+}
+
+int	get_g(int trgb)
+{
+	return (trgb & (0xFF << 8));
+}
+
+int	get_b(int trgb)
+{
+	return (trgb & 0xFF);
+}
