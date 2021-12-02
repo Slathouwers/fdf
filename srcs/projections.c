4c1b20afda80d5607d40b8d69ddf76b1e4d2c10e
Slathouwers <sander.lathouwers@live.be>
2021-12-02 09:08:55 +0000

    added initial projection matrices

---
+++ srcs/projections.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   projections.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 08:14:31 by slathouw          #+#    #+#             */
+/*   Updated: 2021/12/02 08:16:11 by slathouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+t_matr	isometric_proj(double fov_width, double fov_height)
+{
+	return ((t_matr){{
+			{2. / fov_width, 0, 0, 0},
+			{0, 2. / fov_height, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 1}}});
+}
+
+t_matr	perspective_proj(double n, double w, double h, double f)
+{
+	double	l;
+	double	r;
+	double	b;
+	double	t;
+
+	l = -w / 2;
+	r = w / 2;
+	b = -h / 2;
+	t = h / 2;
+	return ((t_matr){{
+			{2 * n / (r - l), 0, (r + l) / (r - l), 0},
+			{0, 2 * n / (t - b), (t + b) / (t - b), 0},
+			{0, 0, -(f + n) / (f - n), -2 * f * n / (f - n)},
+			{0, 0, -1, 0}}});
+}
