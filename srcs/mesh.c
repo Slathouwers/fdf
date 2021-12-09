dc7d1c86fb920b88b63ed1b0831796f05985f2f6
Slathouwers <sander.lathouwers@live.be>
2021-12-09 11:41:02 +0000

    ADD: t_mesh_from_map and center_rotate_map functions to setup and center the mesh struct

--- srcs/mesh.c
+++ srcs/mesh.c
@@ -6,43 +6,11 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:52:48 by slathouw          #+#    #+#             */
-/*   Updated: 2021/12/08 08:40:48 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/12/09 11:35:42 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-
-t_mesh	t_mesh_cube(int size)
-{
-	t_mesh	m;
-	int		i;
-
-	i = -1;
-	t_mesh_init(&m);
-	t_mesh_add_vertex(&m, (t_vect){-1, -1, -1});
-	t_mesh_add_vertex(&m, (t_vect){1, -1, -1});
-	t_mesh_add_vertex(&m, (t_vect){1, 1, -1});
-	t_mesh_add_vertex(&m, (t_vect){-1, 1, -1});
-	t_mesh_add_vertex(&m, (t_vect){-1, -1, 1});
-	t_mesh_add_vertex(&m, (t_vect){1, -1, 1});
-	t_mesh_add_vertex(&m, (t_vect){1, 1, 1});
-	t_mesh_add_vertex(&m, (t_vect){-1, 1, 1});
-	t_mesh_add_edge(&m, (t_point2d){0, 1});
-	t_mesh_add_edge(&m, (t_point2d){1, 2});
-	t_mesh_add_edge(&m, (t_point2d){2, 3});
-	t_mesh_add_edge(&m, (t_point2d){3, 0});
-	t_mesh_add_edge(&m, (t_point2d){4, 5});
-	t_mesh_add_edge(&m, (t_point2d){5, 6});
-	t_mesh_add_edge(&m, (t_point2d){6, 7});
-	t_mesh_add_edge(&m, (t_point2d){7, 4});
-	t_mesh_add_edge(&m, (t_point2d){0, 4});
-	t_mesh_add_edge(&m, (t_point2d){1, 5});
-	t_mesh_add_edge(&m, (t_point2d){2, 6});
-	t_mesh_add_edge(&m, (t_point2d){3, 7});
-	while (++i < m.n_vertices)
-		la_vect_scale(&m.vertices[i], size);
-	return (m);
-}
 
 void	t_mesh_init(t_mesh *m)
 {
@@ -68,4 +36,60 @@ int	t_mesh_add_edge(t_mesh *m, t_point2d e)
 	m->edges = m->arr_edges.data;
 	m->n_edges = m->arr_edges.count;
 	return (m->n_edges);
+}
+
+
+static void center_and_rotate_mesh(t_mesh *m, t_vect size)
+{
+	int i;
+	t_matr rotation;
+	i = -1;
+	while (++i < m->n_vertices)
+	{
+		m->vertices[i].x /= size.x - 1;
+		m->vertices[i].y /= size.y - 1;
+		m->vertices[i] = la_vect_sub(m->vertices[i], (t_vect){.5, .5, 0});
+		m->vertices[i].y *= size.y - 1;
+		m->vertices[i].y /= size.x - 1;
+		m->vertices[i].x *= 500;
+		m->vertices[i].y *= 500;
+		m->vertices[i].z *= 10;
+	}
+	rotation = la_matr_rotation((t_vect) {0, 0, 1}, radians(45));
+	i = -1;
+	while (++i < m->n_vertices)
+	{
+		m->vertices[i] = la_vect_transform(m->vertices[i], rotation);
+	}
+}
+
+
+t_mesh t_mesh_from_map(t_fdf *fdf)
+{
+	t_map *map;
+	int x;
+	int y;
+	int z;
+	t_mesh m;
+
+	y = -1;
+	t_mesh_init(&m);
+	map = &fdf->map;
+	while (++y < map->height)
+	{
+		x = -1;
+		while (++x < map->width)
+		{
+			z = map->z_data[y * map->width + x];
+			t_mesh_add_vertex(&m, (t_vect){x, y, z});
+			if (x > 0)
+				t_mesh_add_edge(&m,
+					(t_point2d){y * map->width + x, y * map->width + x - 1});
+			if (y > 0)
+				t_mesh_add_edge(&m,
+					(t_point2d){y * map->width + x, (y - 1) * map->width + x});
+		}
+	}
+	center_and_rotate_mesh(&m, (t_vect){map->width, map->height, 10.});
+	return (m);
 }
